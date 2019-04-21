a0f105badbff37db7dc9ab42173588eb20edbb30
fabburst <40134787+fabburst@users.noreply.github.com>
2019-04-21 20:43:03 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img width="25%" src="https://tools-tips.com/wp-content/uploads/2017/06/animation.gif">
+  </div>
+</template>
+
+<script>
+import Meme from "./components/Meme";
+
+export default {
+  name: "App",
+  components: {
+    Meme
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
